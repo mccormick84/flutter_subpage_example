3a7a958430628d7219f305c3114956c215598c1a
Kenny <mccormick84@gmail.com>
2022-02-24 15:36:27 +0900

    플러터 네비게이션 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,12 +15,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FirstPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstPage(),
+        '/second': (context) => SecondPage(),
+      },
     );
   }
 }
 
 class FirstPage extends StatefulWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => _FirstPage();
 }
@@ -27,6 +34,43 @@ class FirstPage extends StatefulWidget {
 class _FirstPage extends State<FirstPage> {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sub Page Main'),
+      ),
+      body: Center(
+        child: Text('첫 번째 페이지'),/**/
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // push: 스택에 페이지를 쌓는 역할
+          Navigator.of(context).pushNamed('/second');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // 현재 페이지를 종료
+            },
+            child: Text('돌아가기'),
+          ),
+        ),
+      ),
+    );
   }
 }
